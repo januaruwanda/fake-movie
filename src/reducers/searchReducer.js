b287f7d1a33067fb5c5c52103e951ad1a32b678b
Januar Komar Ruwanda <januar.ruwanda@gmail.com>
2021-09-06 14:53:11 +0700

    fixing up the links warnings

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,38 +1,43 @@
-import { FETCH_MOVIES, SEARCH_MOVIE, FETCH_MOVIE, LOADING } from '../actions/types';
+import {
+  SEARCH_MOVIE,
+  FETCH_MOVIES,
+  FETCH_MOVIE,
+  LOADING,
+} from "../actions/types";
 
 const initialState = {
-    text: '',
-    movies: [],
-    loading: false,
-    movie: []
-}
+  text: "",
+  movies: [],
+  loading: false,
+  movie: [],
+};
 
-export default function(state = initialState, action){
-    switch(action.type){
-        case SEARCH_MOVIE:
-            return{
-                ...state,
-                text: action.payload,
-                loading: false
-            };
-        case FETCH_MOVIES:
-            return{
-                ...state,
-                movies: action.payload,
-                loading: false
-            };
-        case FETCH_MOVIE:
-            return{
-                ...state,
-                movie: action.payload,
-                loading: false
-            };   
-        case LOADING:
-            return{
-                ...state,
-                loading: true
-            };   
-        default:
-            return state
-    }
+export default function searchReducer(state = initialState, action) {
+  switch (action.type) {
+    case SEARCH_MOVIE:
+      return {
+        ...state,
+        text: action.payload,
+        loading: false,
+      };
+    case FETCH_MOVIES:
+      return {
+        ...state,
+        movies: action.payload,
+        loading: false,
+      };
+    case FETCH_MOVIE:
+      return {
+        ...state,
+        movie: action.payload,
+        loading: false,
+      };
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
 }
